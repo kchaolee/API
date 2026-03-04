d7823fb719370eaf27b41208cd077aebdb84aed9
kc.lee <mailto:kchao.lee@gmail.com>
2026-03-04 20:50:20 +0800

    debug aes func

--- src/AesHelperGUI/obj/Debug/net8.0-windows/AesHelperGUI.AssemblyInfo.cs
+++ src/AesHelperGUI/obj/Debug/net8.0-windows/AesHelperGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AesHelperGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1860c976a54c4e64c48b8faacded7974d913ac67")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2cdafdf4d5e1832e0ea6d7a2c251b32f23dc5d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AesHelperGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AesHelperGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
